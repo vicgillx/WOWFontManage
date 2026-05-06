7553527f4b601d49f0c76473bcc6b493cb51fb4d
vicgillx <johnvoid64@gmail.com>
2026-05-06 11:40:28 +0800

    fx

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,3 +1,1 @@
-mod wow;
-
-pub use wow::{apply_font_pack, backup_fonts, get_font_profiles, scan_wow_installations};
+pub mod wow;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,10 +4,10 @@ mod platform;
 pub fn run() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            commands::get_font_profiles,
-            commands::scan_wow_installations,
-            commands::backup_fonts,
-            commands::apply_font_pack
+            commands::wow::get_font_profiles,
+            commands::wow::scan_wow_installations,
+            commands::wow::backup_fonts,
+            commands::wow::apply_font_pack
         ])
         .run(tauri::generate_context!())
         .expect("failed to run WoW Font Manager");
